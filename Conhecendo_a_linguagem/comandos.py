e853b8d52ee05ff8ed08e18ce73d93ee413237e7
Gabriel Braga Gomes <Gabrielbraga1008@gmail.com>
2025-11-13 23:53:25 -0300

    Adicionando algumas anotações

---
+++ Conhecendo_a_linguagem/comandos.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+
+# Modos de impressão;
+
+# Escrevendo Python no terminal, ai ele executa de forma dinamica sem ter que ficar fechando e abrindo o programa para executar novamente de outras maneiras;
+
+# Outra forma é digitando python -i (nome do arquivo) ele executa primeiro e dps ele trava para digitar;
---
+++ Conhecendo_a_linguagem/interativo.py
@@ -0,0 +1,4 @@
+# DIR -> escopo local/lista de metodos que podemos usar;
+
+# Help -> Mostra como funciona a classe que eu to pedindo a ajuda, para sair do help só da enter;
+
