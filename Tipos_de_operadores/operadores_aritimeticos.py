1abc06b2baafb71adcf5f0f49fe89c4041e326f2
GabrielBGomes <Gabrielbraga1008@gmail.com>
2025-11-14 23:28:11 -0300

    Commit 01

---
+++ Tipos_de_operadores/operadores_aritimeticos.py
@@ -0,0 +1,54 @@
+'''
+# Adição
+print(1 + 1)
+
+# Subtração
+print(2 - 1)
+
+# Multiplicação
+print(2 * 2)
+
+# Divisão
+print(10 / 2)
+
+# Divisão inteira
+print(10 // 2)
+
+# Módulo
+print(10 % 3)
+
+# Exponenciação
+print(2 ** 3)
+'''
+# Variáveis
+produto_01 = 10
+produto_02 = 20
+
+
+# Soma
+print(produto_01 + produto_02)
+
+# Subtração
+print(produto_01 - produto_02)
+
+# Multiplicação
+print(produto_01 * produto_02)
+
+# Divisão
+print(produto_01 / produto_02)
+
+# Divisão inteira
+print(produto_01 // produto_02)
+
+# Módulo
+print(produto_01 % produto_02)
+
+# Exponenciação
+print(produto_01 ** produto_02)
+
+x = 10 + 5 * 4
+
+# Forçando uma precedencia diferente
+y = (10 + 5) * 4
+print(x)
+print(y)
---
+++ Tipos_de_operadores/operadores_de_comparacao.py
@@ -0,0 +1,11 @@
+saldo = 200
+saque = 200
+
+# Comparações
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo <= saque)
+print(saldo < saque)
+
